abf912d717683127e04d9adfee92f005c3b052ea
aalesso <aalesso@users.noreply.github.com>
2017-04-29 19:43:34 -0500

    last

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,12 @@
+class DirectorsController < ApplicationController
+
+  def index
+      @directors = Director.all
+      render("directors/index.html.erb")
+    end
+    
+    def show
+      @director = Director.find(params[:id])
+      # render("directors/show.html.erb")
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Routes for the Director resource:
+   # CREATE
+  #  get('/directors/new_form', { :controller => 'directors', :action => 'new_form' })
+  #  get('/create_director', { :controller => 'directors', :action => 'create_row' })
+
+   # READ
+   get('/directors', { :controller => 'directors', :action => 'index' })
+   get('/directors/:id', { :controller => 'directors', :action => 'show' })
+
+   # UPDATE
+  #  get('/directors/:id/edit_form', { :controller => 'directors', :action => 'edit_form' })
+  #  get('/update_director/:id', { :controller => 'directors', :action => 'update_row' })
+
+   # DELETE
+  #  get('/delete_director/:id', { :controller => 'directors', :action => 'destroy' })
+   #------------------------------
+
+
 end
